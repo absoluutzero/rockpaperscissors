c6a3ec306fb30daef18c93e4dc6aae0164bbd345
absoluutzero <absoluutzero@gmail.com>
2022-07-18 05:57:10 -0500

    created initial game function to test and it works in the console

--- script.js
+++ script.js
@@ -8,3 +8,28 @@ function getComputerChoice () {
         return "scissors";
     }    
 }
+
+function Game(playerSelection, computerSelection = getComputerChoice()) {
+    let lower = playerSelection.toLowerCase();
+    if (lower === "rock" && computerSelection === "scissors") {
+        return "player won, rock beats scissors";
+    } else if (lower === "rock" && computerSelection === "paper") {
+        return "computer won, paper beats rock";
+    } else if (lower === "rock" && computerSelection === "rock") {
+        return "Tie, both chose rock";
+    } else if (lower === "paper" && computerSelection === "scissors") {
+        return "computer won, scissors beats paper";
+    } else if (lower === "paper" && computerSelection === "rock") {
+        return "player won, paper beats rock";
+    } else if (lower === "paper" && computerSelection === "paper") {
+        return "Tie! Both chose paper";
+    } else if (lower === "scissors" && computerSelection === "paper") {
+        return "player won, scissors beats paper";
+    } else if (lower === "scissors" && computerSelection === "rock") {
+        return "computer won, rock beats scissors";
+    } else if (lower === "scissors" && computerSelection === "scissors") {
+        return "Tie!, both chose scissors";
+    } else {
+        return "You can't choose that."
+    }
+}
