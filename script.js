f8f067bd1ea8aa33cd24d8da80d2f6cd4cd37437
absoluutzero <absoluutzero@gmail.com>
2022-07-24 10:15:39 -0500

    added buttons and event listeners to call the round fn

--- script.js
+++ script.js
@@ -16,27 +16,36 @@ function playRound(playerSelection, computerSelection = getComputerChoice()) {
     let lower = playerSelection.toLowerCase();
     if (lower === "rock" && computerSelection === "scissors") {
         playerScore = ++playerScore;
+        console.log(playerSelection + computerSelection);
         return "player won, rock beats scissors";
     } else if (lower === "rock" && computerSelection === "paper") {
         computerScore = ++computerScore;
+        console.log(playerSelection + computerSelection);
         return "computer won, paper beats rock";
     } else if (lower === "rock" && computerSelection === "rock") {
+        console.log(playerSelection + computerSelection);
         return "Tie, both chose rock";
     } else if (lower === "paper" && computerSelection === "scissors") {
         computerScore = ++computerScore;
+        console.log(playerSelection + computerSelection);
         return "computer won, scissors beats paper";
     } else if (lower === "paper" && computerSelection === "rock") {
         playerScore = ++playerScore;
+        console.log(playerSelection + computerSelection);
         return "player won, paper beats rock";
     } else if (lower === "paper" && computerSelection === "paper") {
+        console.log(playerSelection + computerSelection);
         return "Tie! Both chose paper";
     } else if (lower === "scissors" && computerSelection === "paper") {
         playerScore = ++playerScore;
+        console.log(playerSelection + computerSelection);
         return "player won, scissors beats paper";
     } else if (lower === "scissors" && computerSelection === "rock") {
         computerScore = ++computerScore;
+        console.log(playerSelection + computerSelection);
         return "computer won, rock beats scissors";
     } else if (lower === "scissors" && computerSelection === "scissors") {
+        console.log(playerSelection + computerSelection);
         return "Tie!, both chose scissors";
     } else {
         return "You can't choose that."
@@ -58,3 +67,12 @@ function Game() {
         console.log("This game was a tie.")
     }
 }
+
+const rock = document.querySelector('#rock');
+rock.addEventListener('click',function(){ playRound("rock")});
+
+const paper = document.querySelector('#paper');
+paper.addEventListener('click',function(){ playRound("paper")});
+
+const scissors = document.querySelector('#scissors');
+scissors.addEventListener('click',function(){ playRound("scissors")});
